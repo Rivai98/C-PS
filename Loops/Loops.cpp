b4e2c5060bd99ba8a353dd0cc70f91276bbbf083
Rivai98 <Omar.18321725@compit.aun.edu.eg>
2025-10-17 20:51:09 +0300

    While Problems

--- Loops/Loops.cpp
+++ Loops/Loops.cpp
@@ -158,52 +158,52 @@ int main()
 //Problem #1:
 //Print diamond
 //● Read an integer N, then print diamond of 2N rows as below
-int N; 
-cin >> N; 
-int stars  , spaces , cnt ;
-int row = 1; 
-while (row <= N) {
-    
-    stars = 2 * row - 1;
-    spaces = N - row; 
-    cnt = 1; 
-    while (cnt <= spaces) {
-        cout << ' '; 
-        ++cnt; 
-    }
-
-    cnt = 1; 
-
-    while (cnt <= stars) {
-        cout << "*"; 
-        ++cnt; 
-    }
-
-    row++;
-    cout << "\n"; 
-}
-
-
-while (row > 0 ) {
-
-    stars = 2 * row - 1;
-    spaces = N - row;
-    cnt = 1;
-    while (cnt <= spaces) {
-        cout << ' ';
-        ++cnt;
-    }
-
-    cnt = 1;
-
-    while (cnt <= stars) {
-        cout << "*";
-        ++cnt;
-    }
-
-    row--;
-    cout << "\n";
-}
+//int N; 
+//cin >> N; 
+//int stars  , spaces , cnt ;
+//int row = 1; 
+//while (row <= N) {
+//    
+//    stars = 2 * row - 1;
+//    spaces = N - row; 
+//    cnt = 1; 
+//    while (cnt <= spaces) {
+//        cout << ' '; 
+//        ++cnt; 
+//    }
+//
+//    cnt = 1; 
+//
+//    while (cnt <= stars) {
+//        cout << "*"; 
+//        ++cnt; 
+//    }
+//
+//    row++;
+//    cout << "\n"; 
+//}
+//
+//row = N; 
+//while (row > 0 ) {
+//
+//    stars = 2 * row - 1;
+//    spaces = N - row;
+//    cnt = 1;
+//    while (cnt <= spaces) {
+//        cout << ' ';
+//        ++cnt;
+//    }
+//
+//    cnt = 1;
+//
+//    while (cnt <= stars) {
+//        cout << "*";
+//        ++cnt;
+//    }
+//
+//    row--;
+//    cout << "\n";
+//}
 
 
 
@@ -233,6 +233,151 @@ while (row > 0 ) {
 //    
 //
 //}
+// =====================================================///
+//Problem #3: Special multiples 2
+//● Read an integer N(1 <= 30) : Print the first N numbers that are
+//○ multiple of 3 but not multiple of 4
+//● Input : 11
+//● Output : 3 6 9 15 18 21 27 30 33 39 42
+//● Notice
+//○ 12 is divisible by both 3 and 4 ⇒ so excluded
+
+    //int N, cnt= 1, number = 0 ; 
+    //cin >> N; 
+    //while (cnt <= N) {
+    //
+    //    if (number % 3 == 0 && number%4 != 0 ) {
+    //         
+    //        cout << number<<" ";
+    //        number++;
+    //        ++cnt; 
+    //    }
+    //    number++; 
+    //    
+    //
+    //}
+
+///secont solution more effection
+//int n, start = 3 ;
+//cin >> n; 
+//while (n) {
+//    if (start % 4 != 0) --n,cout << start << " "; 
+//    start += 3; 
+//}
+
+
+
+
+
+
+//==================================================///
+
+
+//int T; 
+//cin >> T;
+//int N, num , min; 
+//while (T--) {
+//    cin >> N; 
+//    min = INT_MAX; 
+//    while (N--) {
+//        cin >> num; 
+//        if (num < min) {
+//            min = num; 
+//        }
+//
+//    }
+//    cout << min; 
+//
+//    cout << "\n"; 
+//}
+
+//=============================================================///
+
+//Problem #1: Find NOs
+//● Read integer N, then read N strings.
+//○ Print only the strings(of 2 letters).
+//○ These 2 letters must be letter ‘N’ and letter ‘O’
+//■ Regardless of lower or upper case
+//■ Regardless of the 2 letters order
+//■ E.g.print “No”, “ON”, “no”    but ignore e.g.“YEs”, “Noooo”
+//■ That is : a word of 2 letters only N and O
+//● Input
+//○ 9 Yss NO noOO oN Mostafa no nN  oOOooo oO
+//● Output
+//○ NO oN no
+
+//int n, num_os, num_ns; 
+//cin >> n; 
+//while (n) {
+//    string word; 
+//    cin >> word;
+//    int cnt = 0;
+//    num_os = 0; 
+//    num_ns = 0; 
+//    while (cnt < word.length()) {
+//        char character = word[cnt]; 
+//        if (character == 'N' || character == 'n') num_ns++; 
+//        if (character == 'o' || character == 'O') num_os++; 
+//        cnt++; 
+// 
+//    }
+//
+//    if (num_os == 1 && num_ns == 1) {
+//        cout << word << " "; 
+//    }
+//
+//    --n; 
+//
+//
+//
+//}
+//================================================================///
+///
+
+//int n,last_digit ,  reverse = 0 ; 
+//cin >> n; 
+//while (n >  0  ) {
+//
+//    last_digit = n % 10; 
+//    n = n / 10; 
+//    reverse = reverse * 10 + last_digit; 
+//    
+//
+//
+//}
+//cout << reverse << " " << reverse * 3 ;
+
+
+//=====================================================///
+
+int n, m; 
+cin >> n >> m; 
+int cnt = n ; 
+int first_number = 1   ,second_number ;
+while (cnt--) {
+
+    
+    second_number = 1; 
+    while (second_number <= m) {    
+        cout << first_number << " x " << second_number << " = " << first_number * second_number << '\n';
+        second_number++; 
+    }
+
+    cout << "\n"; 
+    first_number++; 
+   
+
+}
+
+
+
+
+
+
+
+
+
+
 return 0;
 
 }
